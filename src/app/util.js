98b93e6cc8ff3f39f611461f9edbd05f51c01bb1
dmytnyk <mytnikda@gmail.com>
2021-11-24 21:06:54 +0200

    Added HW_12

---
+++ src/app/util.js
@@ -0,0 +1,152 @@
+// Standart input definition
+const STDIN = {
+  getStringInput(message, validation) {
+		let input
+		do {
+			input = prompt(message) ?? '';
+		} while (validation(input));
+		return input;
+	},
+                                                   
+	getNumberInput(message, validation) {
+		let input
+		do {
+        input = prompt(message)
+        if (!input) {
+          (input = NaN);
+        } else {
+          input = Number(input);
+        }
+		} while (isNaN(input) || validation(input));
+		return input;
+	},
+}
+
+// DATA definition
+const DATA = {
+	toString() {
+		const now = new Date();
+		return now;
+	},
+	getDateByFormat(format) {
+		const now = new Date();
+
+		switch (format) {
+			case 'mm/dd/yyyy hh:mm':
+				return now.toLocaleString()           //20/11/2021 17:20
+			case 'mm/dd/yyyy':
+				return now.toLocaleDateString()       //20/11/2021
+			case 'Day of the week, dd m yyyy hh:mm':
+				return now.toUTCString()              //Sat 20 Now 2021 18:43:59 GMT
+			case 'yyyy-mm-ddThh:mm:ss.000Z':
+				return now.toISOString()              //2021-11-20T18:43:59.000Z
+			default: 
+				return now.toDateString()             //Sat 20 Now 2021
+		}
+	},
+  getMaxDayOfMonth(year, month) {
+    return new Date (year, month, 0);
+  },
+
+  isLeapYear(year){
+    return this.getMaxDay(year,2).getDate() === 29;
+  },
+ };
+
+// Fabric function for creating users
+const userFabric = function (firstName, lastName, birthYear, birthMonth, birthDay) {
+	return {
+		firstName,
+		lastName,
+    birthYear,
+		birthMonth,
+		birthDay,
+    get fullName() {
+			return `${this.firstName} ${this.lastName}`;
+		},
+	
+		set fullName(value) {
+			const firstName = value.split(' ')[0];
+			const lastName = value.split(' ')[1];
+	
+			if (firstName) {
+				this.firstName = firstName;
+			}
+	
+			if (lastName) {
+				this.lastName = lastName;
+			}
+		},
+   
+		get fullAge() {
+			const actualMonth = birthMonth - 1
+
+			const now = new Date();
+
+			const originBirthDay = new Date(birthYear, actualMonth, birthDay);
+			const birthDate = new Date(now.getFullYear(), actualMonth, birthDay);
+
+			const fullAge = birthDate.getFullYear() - originBirthDay.getFullYear();
+
+			if (now > birthDate) {
+				return fullAge;
+			} else {
+				return fullAge - 1;
+			}	
+		} 
+	}
+}
+
+// Fabric function for creating archive users
+const archiveFabric = function () {
+	const entries = [];
+	const accounts = [];
+	return {
+		add(item) {
+			entries.push(item);
+		},
+		delete(index) {
+			const deleted = entries.splice(index, 1);
+			return deleted[index];
+		},
+		find(cb) {
+    	return entries.find(cb);
+		},
+		filter(cb){
+			return entries.filter(cb)
+		},
+    each(cb){
+      return entries.forEach(cb)
+    },
+    take(start, end) {
+			let newEntries = entries.slice(start, end);
+			return newEntries;   
+		},
+		isEmpty() {
+      if(entries.length <= 0)
+			return true
+		},
+		count() {
+      if(entries.length >= 0)
+			return entries.length;
+		}
+	}
+}
+
+//Fabric function for creating user's account
+// const accountFabric = function () {
+// 	const accounts = [];
+
+// 	return {
+// 		signIn() {
+// 			return account;
+// 		},
+// 		getByEmail(email) {
+// 			const account = this.accounts.find((account) => account.email === email);
+// 			return account;
+// 		},
+// 		signUp() {
+//     	accounts.push(item);
+// 		},
+// 	}
+// }
