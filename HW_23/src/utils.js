7c1c4515ca877884271293d50c36a630efe89073
dmytnyk <mytnikda@gmail.com>
2022-01-18 13:42:53 +0200

    Added HW_23

---
+++ HW_23/src/utils.js
@@ -0,0 +1,27 @@
+export const COOKIE = (() => { 
+  const getCookie = (name) => {
+    for (const pair of document.cookie.split(';')) {
+      const match = `${name}=`;
+      
+      if (pair.trim().startsWith(match)) {
+        return pair.trim().slice(match.length);
+      }
+    }
+
+    return null;
+  };
+
+  const setCookie = (name, value, options = {}) => {
+    let pair = name + '=' + value;
+
+    for (const optionKey of Object.keys(options)) {
+      pair += '; ' + optionKey + '=' + options[optionKey];
+    }
+
+    document.cookie = pair;
+  };
+ 
+  return {
+    setCookie,
+  };
+})();
